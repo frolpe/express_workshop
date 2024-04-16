f46627c95e5c003af4abab2af1dae208688ec0d4
frolpe <frolpe@outlook.com>
2024-04-16 14:24:19 -0600

    add getname

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ app.get("/", (req,res,next)=>{
     res.send("Bienvenido")
 })
 
+app.get("/:name", (req,res,next)=>{
+    res.status(200)
+    res.send("Buenas tardes señor " + req.params.name)
+})
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000,  () => {
     console.log("server is running...")
 });
