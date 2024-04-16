ed34934d8653fee6da0bf284fb1fb4c30fa6b5f5
frolpe <frolpe@outlook.com>
2024-04-16 17:01:15 -0600

    add dir route

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
-const express = require('express');
+const express = require('express')
+const bodyParser = require('body-parser')
 const app = express();
-const { pokemon } = require('./pokedex.json')
+const morgan = require('morgan')
+const pokemon =require("./routes/pokemon")
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", (req,res,next)=>{
     res.status(200).send("Bienvenido al Pokedex")
 })
 
-app.get("/pokemon", (req,res,next)=>{
-    res.status(200).send(pokemon)
-})
-
-app.get("/pokemon/:id([0-9]{1,3})", (req,res,next)=>{
-    const id = req.params.id -1;
-    if(id>0 && id<150){
-        return res.status(200).send(pokemon[req.params.id - 1])
-    }
-    res.status(404).send("El id ingresado no existe")
-
-})
-
-app.get("/pokemon/:name([A-Za-z]+)", (req,res,next) =>{
-    const name = req.params.name;
-
-    const pk = pokemon.filter((p) => {
-        return (p.name.toUpperCase() == name.toUpperCase()) && p;
-    });
-
-    (pk.length>0) ? res.status(200).send(pk) : res.status(404).send("Pokemon no encontrado");
-
-})
+app.use("/pokemon", pokemon)
 
 app.listen(process.env.PORT || 3000,  () => {
     console.log("server is running...")
