6778e59382499c90c660bc4fe8183850281bcb3e
frolpe <frolpe@outlook.com>
2024-04-16 16:02:59 -0600

    add filter search

--- index.js
+++ index.js
@@ -3,18 +3,31 @@ const app = express();
 const { pokemon } = require('./pokedex.json')
 
 app.get("/", (req,res,next)=>{
-    res.status(200)
-    res.send("Bienvenido al Pokedex")
+    res.status(200).send("Bienvenido al Pokedex")
 })
 
 app.get("/pokemon", (req,res,next)=>{
-    res.status(200)
-    res.send(pokemon)
+    res.status(200).send(pokemon)
 })
 
-app.get("/pokemon/:id", (req,res,next)=>{
-    res.status(200)
-    res.send(pokemon[req.params.id - 1])
+app.get("/pokemon/:id([0-9]{1,3})", (req,res,next)=>{
+    const id = req.params.id -1;
+    if(id>0 && id<150){
+        return res.status(200).send(pokemon[req.params.id - 1])
+    }
+    res.status(404).send("El id ingresado no existe")
+
+})
+
+app.get("/pokemon/:name([A-Za-z]+)", (req,res,next) =>{
+    const name = req.params.name;
+
+    const pk = pokemon.filter((p) => {
+        return (p.name.toUpperCase() == name.toUpperCase()) ? p : null;
+    });
+
+    (pk.length>0) ? res.status(200).send(pk) : res.status(404).send("Pokemon no encontrado");
+
 })
 
 app.listen(process.env.PORT || 3000,  () => {
