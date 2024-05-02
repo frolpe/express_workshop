8219922d4f2c6aa806c8ddd3bff81608d7a56a37
frolpe <frolpe@outlook.com>
2024-05-02 13:37:05 -0600

    add function login and token

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const user = express.Router();
 const db = require('../config/database');
+const jwt = require('jsonwebtoken');
 
 user.post("/", async (req,res,next) => {
     const {user_name, user_mail, user_password} = req.body;
@@ -19,12 +20,33 @@ user.post("/", async (req,res,next) => {
 
 });
 
+user.post("/login", async (req,res,next) => {
+    const {user_mail, user_password} = req.body;
+    const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}'`;
+    const rows = await db.query(query);
+
+    if(user_mail && user_password){
+        if(rows.length == 1){
+            const token = jwt.sign({
+                user_id: rows[0].user_id,
+                user_mail: rows[0].user_mail
+            }, "debugkey");
+            return res.status(200).json({code: 200, message: token});
+        } else {
+            return res.status(200).json({code: 200, message: "Usuario y/o contraseña incorrectos"});
+        }
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos"});
+});
+
 user.get("/", async (req,res,next) => {
     const query = "SELECT * FROM user";
     const rows = await db.query(query);
 
     return res.status(200).json({code: 200, message: rows});
 });
+
+
 
 // añade al arbol de dependencias este archivo para despues poderlo importar a cualquier parte del código
 module.exports = user;  
